0c0b2e947e40682fd45a4801d59e5e5e288c6709
water19960630 <83741483+water19960630@users.noreply.github.com>
2021-05-08 20:30:29 +0900

    Add files via upload

---
+++ lessons.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.faq-list-item').click(function() {
+      var $answer = $(this).find('.answer');
+      if($answer.hasClass('open')) {
+        $answer.removeClass('open');
+        $answer.slideUp();
+      } else {
+        $answer.addClass('open');
+        $answer.slideDown();
+      }
+    });
+  });
+  
